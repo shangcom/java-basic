06cdc7d509e9dea7286a1d15020180a038338958
shangcom <dndbs91@gmail.com>
2024-11-26 19:06:26 +0900

    김영한의 실전 자바 - 기본편
    섹션5 - 생성자
    
    완료

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -12,4 +12,22 @@ public class MemberConstruct {
 
         System.out.println("생성자 호출 name=" + name + ", age=" + age + ", grade=" + grade);
     }
+
+    /*
+    초기화되지 않은 필드에 기본값을 제공해야 하는 경우.
+     */
+//    MemberConstruct(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//        this.grade = 50;
+//    }
+
+    /*
+    바로 위 주석처리된 생성자 리팩토링.
+    this() : 현재 클래스의 생성자 호출.
+    생성자 바디의 첫줄에만 작성 가능(아니면 컴파일 에러)
+     */
+    MemberConstruct(String name, int age) {
+        this(name, age, 50);
+    }
 }
